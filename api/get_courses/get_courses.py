bbc6d9dc339348be26b5c8166b6c199984c68b2a
Carlos Hernandez <jchc1987@gmail.com>
2017-11-20 03:45:12 -0600

    core(core): Correct Imports

--- api/get_courses/get_courses.py
+++ api/get_courses/get_courses.py
@@ -18,12 +18,3 @@ def get_courses_handler(event):
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
--- api/get_courses/handler.py
+++ api/get_courses/handler.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import json
 import logging
 from api.get_courses.get_courses import get_courses_handler
