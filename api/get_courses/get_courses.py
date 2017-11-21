468a080529ed9d7c3e5de455584d64f15e0be8a8
Carlos Hernandez <jchc1987@gmail.com>
2017-11-20 18:51:36 -0600

    core(function): Add Response Standar

--- api/get_courses/get_courses.py
+++ api/get_courses/get_courses.py
@@ -6,12 +6,5 @@ log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
 def get_courses_handler(event):
-    # Your code goes here!
-    e = event.get('e')
-    pi = event.get('pi')
 
-    response = {
-        'body': e + pi
-    }
-
-    return response
+    return {'response': 'ok'}
